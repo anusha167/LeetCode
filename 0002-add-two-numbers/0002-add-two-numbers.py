24bf37f25e9df736f7d3b5ff659f20af77bd3b00
Anusha Shinde <anshinde@ucsd.edu>
2026-08-03 17:50:16 -0700

    Time: 3 ms (89.6%), Space: 12.4 MB (91.54%) - LeetHub

--- 0002-add-two-numbers/0002-add-two-numbers.py
+++ 0002-add-two-numbers/0002-add-two-numbers.py
@@ -10,34 +10,22 @@ class Solution(object):
         :type l2: Optional[ListNode]
         :rtype: Optional[ListNode]
         """
-        """
-        pseudocode
-        1. create a dummy starter node
-        2. continue till no digits left
-        3. calculate column sum
-        4. calculate new carry
-        5. the digit we keep is the remainder
-        6. move pointer forward
-        """
 
         dummyHead = ListNode(0)
-        curr = dummyHead
+        tail = dummyHead
         carry = 0
 
-        while l1 is not None or l2 is not None or carry != 0:
-            val1=l1.val if l1 else 0
-            val2=l2.val if l2 else 0
+        while l1 or l2 or carry != 0:
+            digit1 = l1.val if l1 else 0
+            digit2 = l2.val if l2 else 0
 
-            columnSum = val1 + val2 + carry
+            sum = digit1 + digit2 + carry
+            tail.next = ListNode(sum % 10) #Isolate the last digit of the sum to store in the current node (e.g., 14 -> 4)
+            carry = sum // 10 #Isolate the tens digit of the sum to carry over to the next loop (e.g., 14 -> 1)
+            
+            tail = tail.next
 
-            carry = columnSum // 10
-
-            curr.next = ListNode(columnSum % 10)
-
-            curr = curr.next
-            if l1: l1 = l1.next
-            if l2: l2 = l2.next
+            l1 = l1.next if l1 else None
+            l2 = l2.next if l2 else None
 
         return dummyHead.next
-
-        
